107cdd442ca9205adf68b001d86a30f50dcb18c3
Amir Baranes <amirbaranes@fb.com>
2022-02-13 22:16:25 +0200

    FirstApp upload to git

---
+++ iosTestApp/iosFirstTestApp/iosFirstTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosFirstTestApp
+//
+//  Created by Amir Baranes on 13/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
